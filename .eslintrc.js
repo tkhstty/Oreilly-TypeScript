ce1b58dbf1def8718d6b116539dd6873175cc7e0
tkhstty <tkhstty@gmail.com>
2025-08-30 17:28:27 +0900

    First commit
    Setting node, ts, eslint, prettier, and sample code in index.ts

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// .eslintrc.js
+module.esports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ]
+}
